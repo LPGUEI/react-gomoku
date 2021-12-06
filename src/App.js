e0af43551814cbb732d5dbca5552fac6f310e680
LPGUEI <50518097+LPGUEI@users.noreply.github.com>
2021-12-06 14:45:20 -0500

    react-gomoku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Square from "./Components/Square";
+import useBoard from "./useBoard.js";
 
-function App() {
+const Title = styled.h1`
+  text-align: center;
+  font-size: 36px;
+`;
+
+const Message = styled.div`
+  text-align: center;
+  font-size: 24px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  margin: 20px auto;
+  border-radius: 3px;
+  font-size: 18px;
+  padding: 8px;
+  border-radius: 5px;
+  background: grey;
+  color: white;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  text-align: center;
+  margin: 0 auto;
+`;
+
+const Board = styled.div`
+  margin: 0 auto;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+export default function App() {
+  const { board, winner, handlePieceClick, playAgain } = useBoard();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Gomoku</Title>
+      {winner && <Message>The Winner is: {winner}!</Message>}
+      <Button onClick={playAgain}>Play again</Button>
+      <Wrapper>
+        <Board>
+          {board.map((row, rowIndex) => {
+            return (
+              <Row key={rowIndex}>
+                {row.map((col, colIndex) => {
+                  return (
+                    <Square
+                      key={colIndex}
+                      value={board[rowIndex][colIndex]}
+                      row={rowIndex}
+                      col={colIndex}
+                      onClick={handlePieceClick}
+                    />
+                  );
+                })}
+              </Row>
+            );
+          })}
+        </Board>
+      </Wrapper>
     </div>
   );
 }
-
-export default App;
